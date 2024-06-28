f205074d30b68d11c6ae8591b1ed9ab06e32c689
Nicolás Rojas <90432768+NicolasRojas1@users.noreply.github.com>
2024-06-27 20:54:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,8 @@ var repetirTexto = (texto, cantidad = undefined) => {
 
 //repetirTexto("aasas", "a"); 
 
-/* 5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".*/
+/* 5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") 
+devolverá "odnuM aloH".*/
 
 var girarTexto = (texto) => {
 
@@ -135,7 +136,8 @@ var girarTexto = (texto) => {
 
 //girarTexto("1212");
 
-/*6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.*/
+/*6) Programa una función para contar el número de veces que se repite una palabra en un texto largo,
+pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.*/
 
 var contarPalabra = (texto, palabra) => {
     if (!texto) {
@@ -160,7 +162,8 @@ var contarPalabra = (texto, palabra) => {
 
 //contarPalabra("hola mundo adios mundo", "mundo");
 
-/*7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.*/
+/*7) Programa una función que valide si una palabra o frase dada, es un palíndromo 
+(que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.*/
 
 var esPalindromo = (texto) => {
     
@@ -179,7 +182,8 @@ var esPalindromo = (texto) => {
 
 //esPalindromo("salas")
 
-/*8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5. */
+/*8) Programa una función que elimine cierto patrón de caracteres de un texto dado, 
+pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5. */
 
 var eliminarPatron = (texto, patron) => {
 
