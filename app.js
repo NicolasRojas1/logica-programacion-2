0f0108ea69daa200ffee3d7401dea75f1f29faa8
Nicolás Rojas <90432768+NicolasRojas1@users.noreply.github.com>
2024-10-17 12:04:42 -0500

    [UPDATE] ejercicios app.js

--- app.js
+++ app.js
@@ -173,24 +173,20 @@ const numeroAleatorio = (min = undefined, max = undefined) => {
 //numeroAleatorio(501, 600);
 /*10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.*/
 
-const numeroCapicua = (numero = undefined)  => {
-    if (numero === undefined) {
-        return console.warn('No ingresaste un valor');
-    }
-    if (isNaN(numero)) {
-        return console.error('El valor a ingresar debe ser un numero');
-    }
+const numeroCapicua = (numero = undefined) => {
+    if (numero === undefined) return console.warn('No ingresaste un valor');
+
+    if (typeof numero !== 'number') return console.error('El valor a ingresar debe ser un numero');
+    
     let numeroStr = numero.toString();
     let numeroGirado = numeroStr.split("").reverse().join("");
-    
-    if (numeroStr === numeroGirado) {
-        return console.info(`${numeroStr} = ${numeroGirado} es capicúa`)
-    } else {
-        return console.error('El numero no es capicúa');
-    }
+
+    (numeroStr === numeroGirado) 
+    ? console.info(`${numeroStr} = ${numeroGirado} es capicúa`)
+    : console.error('El numero no es capicúa');
 }
 
-//numeroCapicua(202.202);
+numeroCapicua(202.202);
 
 /*11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.*/
 
