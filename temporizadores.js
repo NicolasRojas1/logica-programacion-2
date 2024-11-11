60bc0d9fb02b8e94e0a169b241df5081bcf0d69d
NicolasRojas <nicrojasc6@gmail.com>
2024-11-11 15:56:40 -0500

    [ADD] aprendiendo conceptos de programacion asincrona

--- temporizadores.js
+++ temporizadores.js
@@ -1,5 +1,25 @@
 //Programacion Asincrona
 
-// ----- Temporizadores ------
+/*
+Procesamiento Single thread y multi thread
+
+JS es single thread, un solo hilo. No se tiene que tener preocupaciones por concurrencia como en otros lenguajes
+
+Como contra es que tampoco se pueden realizar operaciones largas sin que se bloquee el hilo principal
+
+De ahi viene la sincronia, que permite realizar largas solicitudes de red sin bloquear el hilo principal
+
+- Hilos: Son la unidades básicas de ejecución de cada proceso que realiza nuestra máquina. 
+
+ JS trabaja bajo el concepto de LIFO: Last in - First out
+
+ - Operaciones de CPU: Son aquellas que van a pasar el mayor tiempo consumiendo los procesos de la CPU
+
+ - Operaciones de Entrada y Salida (Input / Output): Son aquellas que pasarán el tiempo esperando la petición del recurso que han solicitado
+
+- Operaciones Concurrentes: Es cuando 2 o más tareas progresan simultáneamente. La clave es la palabra progresar, una tarea pudo empezar antes o después. (Varias tareas progresan al mismo tiempo)
+
+- Operaciones Paralelas: Es cuando 2 o más tareas se ejecutan al mismo tiempo
+*/
 
 
