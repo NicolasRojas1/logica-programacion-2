7a2053941921c9db45cb4ae3c949b11768b06ff8
NicolasRojas <nicrojasc6@gmail.com>
2024-11-29 18:33:46 -0500

    [ADD] DOM: Nodos, elementos y selectores

--- js/dom.js
+++ js/dom.js
@@ -1,35 +1,36 @@
-//Accediendo a los nodos del dom
-console.log(document.head);
+// //Accediendo a los nodos del dom
+// console.log(document.head);
 
-//Accediendo en la parte del body
-console.log(document.body);
+// //Accediendo en la parte del body
+// console.log(document.body);
 
-//Mapeo del DOM completo (el tipo del documento)
-console.log(document.doctype);
+// //Mapeo del DOM completo (el tipo del documento)
+// console.log(document.doctype);
 
-//Titulo
-console.log(document.title);
+// //Titulo
+// console.log(document.title);
 
-//Juego de caracteres
-console.log(document.characterSet);
+// //Juego de caracteres
+// console.log(document.characterSet);
 
-//Acceder a enlaces del documento
-console.log(document.links);
+// //Acceder a enlaces del documento
+// console.log(document.links);
 
-//Acceder a las imagenes
-console.log(document.images);
+// //Acceder a las imagenes
+// console.log(document.images);
 
-//Acceder a una coleccion de los formularios que tenga el documento
-console.log(document.forms);
+// //Acceder a una coleccion de los formularios que tenga el documento
+// console.log(document.forms);
 
-//Acceder a las hojas de estilo del documento
-console.log(document.styleSheets);
+// //Acceder a las hojas de estilo del documento
+// console.log(document.styleSheets);
 
-//Acceder a los scripts
-console.log(document.scripts);
+// //Acceder a los scripts
+// console.log(document.scripts);
 
-//Mapear algo que se selecciona en el navegador
-setTimeout(() => {
-    console.log(document.getSelection().toString());
-}, 3000);
+// //Mapear algo que se selecciona en el navegador
+// setTimeout(() => {
+//     console.log(document.getSelection().toString());
+// }, 3000);
 
+//NODOS, ELEMENTOS Y SELECTORES
