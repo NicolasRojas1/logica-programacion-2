55a551110d5605ac3fbfa0b7ce706f8e68042117
NicolasRojas <nicrojasc6@gmail.com>
2025-01-27 16:08:12 -0500

    [UPDATE] learning about DOM

--- js/dom.js
+++ js/dom.js
@@ -36,26 +36,29 @@
 //NODOS, ELEMENTOS Y SELECTORES
 //no confundir una etiqueta de html con un nodo, los comentarios son nodos, las etiquetas son otro tipo de nodos, los textos como tal también son nodos
 
-console.log(document.getElementById("menu"));
+// console.log(document.getElementById("menu"));
 
 //Este es mas lento que el getElementById.Solo trae el primer elemento que encuentre con el id
-console.log(document.querySelector("#menu"));
+// console.log(document.querySelector("#menu"));
 
 //Para buscar todos los elementos con este tipo de id
-console.log(document.querySelectorAll("#menu"));
+// console.log(document.querySelectorAll("#menu"));
 
 //Trae solo el primer a
-console.log(document.querySelector("a"));
+// console.log(document.querySelector("a"));
 
 //Trae todos los elementos en una nodelist
-console.log(document.querySelectorAll("a"));
+// console.log(document.querySelectorAll("a"));
 
 //capturando la informacion de cada elemento
-document.querySelectorAll("a").forEach((ele) => console.log(ele));
+// document.querySelectorAll("a").forEach((ele) => console.log(ele));
 
 //Como es una especie de arreglo, esta nodeList puedo acceder a la posicion de algún elmento con la notación de los corchetes
-console.log(document.querySelectorAll(".card")[1]);
+// console.log(document.querySelectorAll(".card")[1]);
 
 //Traer una serie especifica de li, como en el documento hay mas de 10. Yo solo quiero las etiquetas con id de menu
-console.log(document.querySelectorAll("#menu li"));
+// console.log(document.querySelectorAll("#menu li"));
+
+//Forma de llamar atributos y establecesr valores
+console.log(document.documentElement.lang); //Trae la del lenguaje
 
